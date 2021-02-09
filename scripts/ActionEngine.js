14dc4b32be26b51d58a9fc76b66d75b0422f9b55
KGayatri15 <gayatriinshunya@gmail.com>
2021-02-09 19:33:37 +0530

    SIGNUP added

--- scripts/ActionEngine.js
+++ scripts/ActionEngine.js
@@ -8,20 +8,26 @@ const states = Object.freeze({
 class ActionEngine{
     constructor(options){
         this.actionSteps = options.actionSteps;
-        this.actionStepData = new Map(),
+        this.actionStepData = {},
         this.actionStepsExecuted=[],
         this.index;
         console.log(this.actionSteps);
+        this.intialise();
         this.executeActionSteps()
     }
     ActionStepError(index,exception){
         this.updateActionStepState(index,states["e"]);
-        console.log("An exception " + exception + " while performing the task " + index);
+        console.log("An exception " + exception + " while performing the task " + this.actionSteps[index]['actionStepIndex']);
     }
-    updateStartActionStep(){
-       console.log("Updating state of actionSteps Index " + this.index);
-       this.actionSteps[this.index]['state'] = states["0."];
+    updateActionStepArguments(data){
+       console.log("Updating arguments of actionSteps Index " + this.index);
+       this.includeArguments(this.index,data,"Include");
+       this.updateActionStepState(this.index,states["0."]);
     }
+    updateStateOfActionStep(){
+        console.log("Updating state of actionSteps Index " + this.index);
+        this.updateActionStepState(this.index,states["0."]);
+     }
     updateActionStepState(index,state){
         this.actionSteps[index]['state'] = state;
     }   
@@ -40,22 +46,20 @@ class ActionEngine{
                 var checkSubset = conditionExists|| operate.isEqualStrict(this.actionSteps[i]['condition']['completedActionSteps'],undefined) || operate.hasAllof(this.actionSteps[i]['condition']['completedActionSteps'],this.actionStepsExecuted);
                 var comparisonsCorrect = conditionExists|| operate.isEqualStrict(this.actionSteps[i]['condition']['compare'],undefined)|| this.compareValues(this.actionSteps[i]['condition']['compare']);
                 if(checkSubset && comparisonsCorrect){
-                        var input, noInput = false;
-                        var argumentsExist = operate.isEqualStrict(this.actionSteps[i]['arguments'],undefined);
-                        var requiredArgumentsExist = operate.isEqualStrict(this.actionSteps[i]['fromPrevious'],undefined);
-                        if(argumentsExist && requiredArgumentsExist )
+                        var  noInput = false;
+                        var Noarguments = operate.isEqualStrict(this.actionSteps[i]['arguments'],undefined);
+                        var NofromPrevious = operate.isEqualStrict(this.actionSteps[i]['fromPrevious'],undefined);
+                        if(Noarguments && NofromPrevious && operate.isEqualStrict(this.actionStepData[this.actionSteps[i]['actionStepIndex']]['arguments'],{}))
                             noInput = true;
-                        else if(argumentsExist)
-                            input = this.includeArguments({},this.actionSteps[i]['fromPrevious'])
-                        else if(requiredArgumentsExist)
-                            input = this.includeArguments(this.actionSteps[i]['arguments'],{})
-                        else
-                            input = this.includeArguments(this.actionSteps[i]['arguments'],this.actionSteps[i]['fromPrevious'])
+                        if(!Noarguments)
+                            this.includeArguments(i,this.actionSteps[i]['arguments'],'Include')
+                        if(!NofromPrevious)
+                            this.includeArguments(i,this.actionSteps[i]['fromPrevious'],'Previous')
                         try{
                             if(noInput)
-                                this.actionStepData[this.actionSteps[i]['actionStepIndex']] = this.actionSteps[i]['method'].call(this);
+                                this.actionStepData[this.actionSteps[i]['actionStepIndex']]['output'] = this.actionSteps[i]['method'].call(this);
                             else
-                                this.actionStepData[this.actionSteps[i]['actionStepIndex']] = this.actionSteps[i]['method'].call(this,input);
+                                this.actionStepData[this.actionSteps[i]['actionStepIndex']]['output'] = this.actionSteps[i]['method'].call(this,this.actionStepData[this.actionSteps[i]['actionStepIndex']]['arguments']);
                             this.actionStepsExecuted.push(this.actionSteps[i]['actionStepIndex']);
                             this.updateActionStepState(i,states["1"]);
                         }catch(exception){
@@ -73,11 +77,14 @@ class ActionEngine{
     sleep(ms) {
         return new Promise(resolve => setTimeout(resolve, ms));
     }
-    includeArguments(arg,obj){
+    includeArguments(index,obj,from){
         for(var key in obj){
-                arg[key] = this.actionStepData[obj[key]];
+            if(operate.isEqualStrict(from,"Previous"))
+                this.actionStepData[this.actionSteps[index]['actionStepIndex']]['arguments'][key]  = this.actionStepData[obj[key]]['output'];
+            else if(operate.isEqualStrict(from,"Include"))
+                this.actionStepData[this.actionSteps[index]['actionStepIndex']]['arguments'][key] = obj[key];
         }
-        return arg;
+        console.log("Included arguments as mentioned");
     }
     compareValues(comparisons){
         var count = 0;
@@ -94,5 +101,12 @@ class ActionEngine{
         if(operate.isEqualStrict(count,comparisons.length))
             return true;
         return false;
+    }
+    intialise(){
+        for(var i=0;i< this.actionSteps.length;i++){
+            this.actionStepData[this.actionSteps[i]['actionStepIndex']] = {};
+            this.actionStepData[this.actionSteps[i]['actionStepIndex']]['arguments']= {};
+        }  
+        console.log("Initialization Done !");
     }
 }
--- scripts/controller.js
+++ scripts/controller.js
@@ -1,24 +1,65 @@
-var loginActionFlow;
-function loginflow(event){
+var loginActionFlow,signUpActionFlow;
+//SIGNUP
+function signup(event){
     event.preventDefault();
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
+    signUpActionFlow = new ActionEngine({
+        actionSteps:[
+            {
+                actionStepIndex:1,
+                method:checkUserExists,
+                state:states["shunya"],
+            },
+            {
+                actionStepIndex:2,
+                method:alertAboutSignUp,
+                state:states["0."],
+                condition:{
+                    completedActionSteps:[1]
+                },
+                fromPrevious:{
+                    "exist":1
+                }
+            }
+        ]
+    })
+}
+function startExecutionSignupFlow(event){
+    event.preventDefault();
+    var username = document.getElementById('Rusername').value;
+    var password = document.getElementById('Rpassword').value;
     console.log("Username:- " + username + ":::Password:- " + password);
     var data = {
         "username":username,
         "password":password
     }
+    signUpActionFlow.updateActionStepArguments(data);
+}
+function checkUserExists(data){
+    if(localStorage.getItem(data.username)=== null){
+        localStorage.setItem(data.username,data.password);
+        return false;
+    }else
+        return true;
+}
+function alertAboutSignUp(data){
+    if(data.exist)
+        alert('Username already exists');
+    else
+        alert("Credentials has been registered");
+}
+//LOGIN
+function login(event){
+    event.preventDefault();
     loginActionFlow = new ActionEngine({
         actionSteps:[
             {
                 actionStepIndex:1,
                 method:checkCredetials,
                 state:states["shunya"],
-                arguments:data,
             },
             {
                 actionStepIndex:2,
-                method:this.alertAbout,
+                method:alertAboutLogin,
                 state:states["0."],
                 condition:{
                     completedActionSteps:[1]
@@ -30,14 +71,25 @@ function loginflow(event){
         ]
     })
 }
+function startExecutionLoginFlow(event){
+    event.preventDefault();
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    console.log("Username:- " + username + ":::Password:- " + password);
+    var data = {
+        "username":username,
+        "password":password
+    }
+    loginActionFlow.updateActionStepArguments(data);
+}
 function checkCredetials(data){
-    if(data.username === 'ae'&& data.password === 'ae'){
+    if(localStorage.getItem(data.username)=== data.password){
         return true;
     }else{
         return false;
     }
 }
-function alertAbout(data){
+function alertAboutLogin(data){
     if(data.exist){
        alert('Entered correct crentials. :-)');
     }else if(!data.exist){
